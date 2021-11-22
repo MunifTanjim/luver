ff808ace75d3f9ccf8d608b2b4ac307c589da93c
Munif Tanjim <hello@muniftanjim.dev>
2021-11-22 22:55:52 +0600

    feat: add zsh entrypoint

---
+++ luver.plugin.zsh
@@ -0,0 +1,14 @@
+export LUVER_SRC="${0:A:h}"
+export LUVER_DIR="${LUVER_DIR:-"${XDG_DATA_HOME:-"${HOME}/.local/share"}/luver"}"
+export LUVER_MULTISHELL_PATH="$(mktemp --dry-run /tmp/luver_multishells/${$}_$(date +%s%3N)_XXXXXXX)"
+export PATH="${LUVER_MULTISHELL_PATH}/bin:${LUVER_SRC}/bin:${PATH}"
+
+if ! test -d /tmp/luver_multishells; then
+  mkdir /tmp/luver_multishells
+fi
+
+if test -d ${LUVER_DIR}/aliases/default; then
+  ln -s ${LUVER_DIR}/aliases/default/installation ${LUVER_MULTISHELL_PATH}
+fi
+
+rehash
