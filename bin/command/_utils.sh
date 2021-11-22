08e704592951e5770de331cef064f2c9df3e76f0
Munif Tanjim <hello@muniftanjim.dev>
2021-11-23 00:43:19 +0600

    refactor(list): extract version and aliasmap list utils

--- bin/command/_utils.sh
+++ bin/command/_utils.sh
@@ -21,3 +21,37 @@ function luver_is_version_installed() {
   local -r version="${1}"
   test -d "${LUVER_DIR}/lua-versions/${version}"
 }
+
+function luver_list_version() {
+  if test -d "${LUVER_DIR}/lua-versions"; then
+    local version_dir version
+
+    for version_dir in ${LUVER_DIR}/lua-versions/*; do
+      version="$(basename "${version_dir}")"
+
+      if test "${version}" = "*"; then
+        continue
+      fi
+      
+      echo "${version}"
+    done
+  fi
+}
+
+function luver_list_aliasmap() {
+  if test -d "${LUVER_DIR}/aliases"; then
+    local alias_dir alias version
+
+    for alias_dir in ${LUVER_DIR}/aliases/*; do
+      alias="$(basename "${alias_dir}")"
+
+      if test "${alias}" = "*"; then
+        continue
+      fi
+
+      version="$(basename $(realpath "${alias_dir}"))"
+
+      echo "${alias}:${version}"
+    done
+  fi
+}
