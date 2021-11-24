a4d23f654f0fa37e511d080070c5397f58eaa475
Munif Tanjim <hello@muniftanjim.dev>
2021-11-24 14:18:26 +0600

    feat(entrypoint): prepare for multi tool support

--- luver.plugin.zsh
+++ luver.plugin.zsh
@@ -1,14 +1,10 @@
 export LUVER_SRC="${0:A:h}"
 export LUVER_DIR="${LUVER_DIR:-"${XDG_DATA_HOME:-"${HOME}/.local/share"}/luver"}"
-export LUVER_MULTISHELL_PATH="$(mktemp --dry-run /tmp/luver_multishells/${$}_$(date +%s%3N)_XXXXXXX)"
-export PATH="${LUVER_MULTISHELL_PATH}/bin:${LUVER_SRC}/bin:${PATH}"
+export LUVER_CURRENT_DIR="$(mktemp --dry-run /tmp/luver_current_${$}_$(date +%s)_XXX)"
+export PATH="${LUVER_CURRENT_DIR}/bin:${LUVER_SRC}/bin:${PATH}"
 
-if ! test -d /tmp/luver_multishells; then
-  mkdir /tmp/luver_multishells
-fi
-
-if test -d ${LUVER_DIR}/aliases/default; then
-  ln -s ${LUVER_DIR}/aliases/default/installation ${LUVER_MULTISHELL_PATH}
+if test -d ${LUVER_DIR}/lua-aliases/default; then
+  ln -s ${LUVER_DIR}/lua-aliases/default/installation ${LUVER_CURRENT_DIR}
 fi
 
 rehash
