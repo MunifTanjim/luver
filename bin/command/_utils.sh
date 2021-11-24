b4c4f3fb39b33b8aa4aae16e04b4aee67c2201dc
Munif Tanjim <hello@muniftanjim.dev>
2021-11-24 14:32:23 +0600

    feat(list): prepare for multi tool support

--- bin/command/_utils.sh
+++ bin/command/_utils.sh
@@ -1,37 +1,3 @@
 function luver_get_lua_versions() {
   curl -fSsL https://www.lua.org/ftp | sed -n "s/.*>lua-\(.*\..*\).tar.gz<.*/\1/p" | sort
 }
-
-function luver_list_version() {
-  if test -d "${LUVER_DIR}/lua-versions"; then
-    local version_dir version
-
-    for version_dir in ${LUVER_DIR}/lua-versions/*; do
-      version="$(basename "${version_dir}")"
-
-      if test "${version}" = "*"; then
-        continue
-      fi
-      
-      echo "${version}"
-    done
-  fi
-}
-
-function luver_list_aliasmap() {
-  if test -d "${LUVER_DIR}/aliases"; then
-    local alias_dir alias version
-
-    for alias_dir in ${LUVER_DIR}/aliases/*; do
-      alias="$(basename "${alias_dir}")"
-
-      if test "${alias}" = "*"; then
-        continue
-      fi
-
-      version="$(basename $(realpath "${alias_dir}"))"
-
-      echo "${alias}:${version}"
-    done
-  fi
-}
