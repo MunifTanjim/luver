7319fd88f0c764fdfbf055506aec71a05980c989
Munif Tanjim <hello@muniftanjim.dev>
2021-11-27 13:23:52 +0600

    test(entrypoint): add tests for bash

---
+++ tests/entrypoint/bash.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+declare root_dir="$(git rev-parse --show-toplevel)"
+source ${root_dir}/luver.bash
+
+echo "should set LUVER_SRC"
+test -n "${LUVER_SRC}"
+
+echo "should set LUVER_SRC to repository root"
+test "${LUVER_SRC}" = "${root_dir}"
+
+echo "should set LUVER_DIR"
+test -n "${LUVER_DIR}"
+
+echo "should set LUVER_CURRENT_DIR"
+test -n "${LUVER_CURRENT_DIR}"
+
+echo "should include current pid in LUVER_CURRENT_DIR"
+echo "${LUVER_CURRENT_DIR}" | grep -q ${$}
+
+echo "should add LUVER_CURRENT_DIR/bin to PATH"
+echo "${PATH}" | tr ':' '\n' | grep -q "${LUVER_CURRENT_DIR}/bin"
+
+echo "should make luver command available"
+type luver >/dev/null
